79e0197aff5d15e83b83e7da40e5a60ef342d2f7
isJinhonglu <lujinhong@westlake.edu.cn>
2022-05-16 19:38:01 +0800

    update

--- strelka2/download.sh
+++ strelka2/download.sh
@@ -9,3 +9,7 @@ gzip -d Homo_sapiens_assembly38.fasta.gz
 ## Creating the fasta index file
 gsutil cp -R gs://genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.fai .
 # OR use: samtools Homo_sapiens_assembly38.fasta
+
+## Creating the WESinterval
+bgzip -c <(zcat S31285117_Padded.list.gz |sed 's/[:,-]/\t/g') > region.bed.gz
+tabix -s 1 -b 2 -e 3 region.bed.gz
