8a0e7efb6377bfaf0ac4d117ce4f6b94096c9c88
xiayh17 <xiayh17@gmail.com>
2022-09-01 13:29:26 +0800

    update scripts

--- dndscv/workflow/scripts/download.sh
+++ dndscv/workflow/scripts/download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
+mkdir -p resources;
 cd resources;
 
 ## Downloading RefCDS objects to run dndscv
-wget https://github.com/im3sanger/dndscv_data/blob/master/data/RefCDS_human_GRCh38_GencodeV18_recommended.rda
+wget -N https://github.com/im3sanger/dndscv_data/blob/master/data/RefCDS_human_GRCh38_GencodeV18_recommended.rda
--- dndscv/workflow/scripts/drivergene.R
+++ dndscv/workflow/scripts/drivergene.R
@@ -1,9 +1,15 @@
+## set up packages
+options(repos = c(CRAN = "http://cran.rstudio.com"))
+if (!requireNamespace("pak", quietly = TRUE))
+  install.packages("pak")
+if (!requireNamespace("dndscv", quietly = TRUE))
+  pak::pkg_install("im3sanger/dndscv",ask = FALSE)
+
 library("dndscv")
 
-
 args <- commandArgs()
-
-mutations <- read.csv(file = args[6],sep = "\t",header = FALSE,col.names = c("sampleID","chr","pos","ref","mut"))
+mutations <- read.csv(file = "output/drivergene.input",sep = "\t",header = FALSE,col.names = c("sampleID","chr","pos","ref","mut"))
+mutations$chr = gsub("chr","",mutations$chr)
 dndsout = dndscv(mutations,refdb = args[7] , cv=NULL)
 sel_cv = dndsout$sel_cv
 signif_genes = sel_cv[sel_cv$qglobal_cv<0.1, c("gene_name","qglobal_cv")]
